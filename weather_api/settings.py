95626e3100467342d6940444c971bf0c97050704
LimViboth <limvibothjr@gmail.com>
2025-08-15 22:22:52 +0700

    Update settings.py

--- weather_api/settings.py
+++ weather_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-m#)q@_n6r6j^t1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*').split('')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*').split(",")]
 
 
 # Application definition
