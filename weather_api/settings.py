6fff4fe9f42f4184a0dad50471291670b7200a23
LimViboth <limvibothjr@gmail.com>
2025-08-15 21:41:16 +0700

    Update settings.py

--- weather_api/settings.py
+++ weather_api/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m#)q@_n6r6j^t1u&eb7%*v5p+67nr&48l(iglwbq)n58n%%via'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-m#)q@_n6r6j^t1u&eb7%*v5p+67nr&48l(iglwbq)n58n%%via')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*').split('')]
 
 
 # Application definition
